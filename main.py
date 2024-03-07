270dde670074df4bdda7640368fd673179b00784
Adriel Soares <adriellsoares067@gmail.com>
2024-03-07 19:08:18 -0400

    .

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-for i in range(10):
-    print("Teste")
+import customtkinter as ctk
+
+
+root = ctk.CTk()
+root.geometry("1000x600")
+root.title("Gerenciamento")
+
+
+bar_i = ctk.CTkFrame(master=root,
+                     bg_color="red",
+                     fg_color="red",
+                     width=100,
+                     height=1000
+                     )
+bar_i.place(x=0,y=0)
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
