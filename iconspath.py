ab50ea9a5767371f94fa4c5c8597247ec2dcd4a3
Adriel Soares <adriellsoares067@gmail.com>
2024-03-08 10:05:49 -0400

    Criação da sidebar

--- iconspath.py
+++ iconspath.py
@@ -1,8 +1,13 @@
 from customtkinter import CTkImage
-from PIL import Image
+from PIL import Image, ImageOps
+
+
 
 img = Image.open(r"icons\menu.png")
 SIDEBAR_ICON = CTkImage(light_image=img, size=(32, 31))
 
 img1 = Image.open(r'icons\icons8-settings-256.png')
 SETTINGS_ICON = CTkImage(light_image=img1, size=(32, 31))
+
+img2 =  Image.open(r'icons\icons8-close-250.png')
+CLOSE_ICON = CTkImage(light_image=img2, size=(32, 31))
--- main.py
+++ main.py
@@ -9,46 +9,64 @@ class ConfigEdge(tk.Toplevel):
         self.geometry("300x300")
         self.title("Configurações")
         self.resizable(False, False)
-        self.configure(bg="")
+        self.tema_atual = "#171717"
+        self.configure(bg=self.tema_atual)
         self.iconbitmap(r"icons\icons8-settings-256.ico")
-        self.tema_atual = "#0d9488"
+        self.switch_var = ctk.StringVar(value="#171717")
         
-
+        
+        self.mudar_tema()
         tema = ctk.CTkSwitch(self,
-                             onvalue="#282424",
-                             offvalue="#d4d4d8",
-                             ).pack()
+                             text="",
+                             command=self.mudar_tema,
+                             variable=self.switch_var,
+                             onvalue="#171717",
+                             offvalue="#d4d4d4"
+                             )
+        tema.place(x=130,y=3)
+
+    def mudar_tema(self):
+        self.tema_atual = self.switch_var.get()
+        self.configure(bg=self.tema_atual)
+    
+
+    def get_tema_atual(self):
+        return self.switch_var.get()
 
 
 class Gestor(tk.Tk):
 
-    def __init__(self, config):
+    def __init__(self):
         super().__init__()
         self.geometry("1000x600")
         self.title("Gerenciamento")
         self.resizable(False, False)
-        self.tema_atual = config.tema_atual
+        self.tema_atual = "#171717"
         self.configure(bg=self.tema_atual)
+        self.bar_status = 'reduced'
         
         # Fundo da barra lateral inicial
-        sidebar_bg = tk.Frame(self,
-                              bg=self.tema_atual,
+        self.sidebar_bg = tk.Frame(self,
+                              bg='#0d9488',
                               width=52,
                               height=1000)
-        sidebar_bg.place(x=0, y=0)
+        self.sidebar_bg.place(x=0, y=0)
+        
 
         # Botão da barra lateral
-        sidebar_btn = ctk.CTkButton(master=sidebar_bg,
+        self.sidebar_btn = ctk.CTkButton(master=self.sidebar_bg,
                                     width=45,
                                     height=45,
                                     text="",
                                     image=iconspath.SIDEBAR_ICON,
                                     fg_color="transparent",
-                                    hover_color="#115e59")
-        sidebar_btn.place(x=2, y=3)
+                                    hover_color="#115e59",
+                                    command=self.onclick,
+                                    )
+        self.sidebar_btn.place(x=2, y=3)
 
         # Botão de configurações
-        config_btn = ctk.CTkButton(master=sidebar_bg,
+        self.config_btn = ctk.CTkButton(master=self.sidebar_bg,
                                    width=45,
                                    height=45,
                                    text="",
@@ -57,12 +75,56 @@ class Gestor(tk.Tk):
                                    hover_color="#115e59",
                                    command=self.open_config,
                                    )
-        config_btn.place(x=2, y=553)
+        self.config_btn.place(x=2, y=553)
+
+
+        self.close_sidebar = ctk.CTkButton(self.sidebar_bg,
+                                           width=45,
+                                           height=45,
+                                           fg_color="transparent",
+                                           hover_color="#115e59",
+                                           text="",
+                                           image=iconspath.CLOSE_ICON,
+                                           command=self.onclick,
+                                           )
+        self.close_sidebar.place(x=250,y=4)
+
 
 
     def open_config(self):
         config = ConfigEdge(self)
         config.grab_set()
+
+        def on_switch_change(*args):
+            tema_atual_edge = config.get_tema_atual()
+            self.tema_atual = tema_atual_edge
+            self.configure(bg=self.tema_atual)
+        config.switch_var.trace("w", on_switch_change)
+    
+
+    def expand(self):
+        self.sidebar_bg.configure(width=300)
+        self.bar_status = "expanded"
+        self.sidebar_btn.place_forget()
+    
+
+    def reduce(self):
+        self.sidebar_bg.configure(width=52)
+        self.bar_status = "reduced"
+        self.sidebar_btn.place(x=2, y=3)
+
+
+    def onclick(self):
+        if self.bar_status == "reduced":
+            self.expand()
+        elif self.bar_status == "expanded":
+            self.reduce()
+
+        
+        
+        
+
+
 
 if __name__ == "__main__":
     gestor = Gestor()
