2414bc38142d74c3bb4f643faa13c35efaa52f97
Adriel Soares <adriellsoares067@gmail.com>
2024-03-08 21:57:04 -0400

    .

--- iconspath.py
+++ iconspath.py
@@ -14,3 +14,9 @@ CLOSE_ICON = CTkImage(light_image=img2, size=(32, 31))
 
 img3 = Image.open(r'icons\icons8-admin-90.png')
 ADMIN_ICON = CTkImage(light_image=img3, size=(32, 31))
+
+img4 = Image.open(r'icons\icons8-eye-90.png')
+EYE_ICON = CTkImage(light_image=img4, size=(15, 15))
+
+img5 = Image.open(r'icons\icons8-closed-eye-50.png')
+CLOSED_EYE_ICON = CTkImage(light_image=img5, size=(15, 15))
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class ConfigEdge(tk.Toplevel):
         self.title("Configurações")
         self.resizable(False, False)
         self.tema_atual = "#171717"
-        self.configure(bg=self.tema_atual)
+        self.configure(bg=self.tema_atual)  # Set background color
         self.iconbitmap(r"icons\icons8-settings-256.ico")
         self.switch_var = ctk.StringVar(value="#171717")
         
@@ -24,7 +24,8 @@ class ConfigEdge(tk.Toplevel):
                              onvalue="#171717",
                              offvalue="#d4d4d4"
                              )
-        tema.place(x=130,y=3)
+        tema.place(x=130, y=3)
+
 
     def mudar_tema(self):
         self.tema_atual = self.switch_var.get()
@@ -42,14 +43,109 @@ class AuthAdmin(tk.Toplevel):
         super().__init__(parent)
         self.geometry("300x300")
         self.title("Admin Login")
-        self.configure(bg=tema)
+        self.configure(bg='#171717')
+        self.resizable(False, False)
+        self.senha_oculta = True
 
+        #Entry do usuário
+        self.user_entry = ctk.CTkEntry(self,
+                                   width=200,
+                                   height=40,
+                                   placeholder_text="Usuário",
+                                   placeholder_text_color='#0d9488',
+                                   font=("Arial", 14),
+                                   border_width=2,
+                                   border_color='#0d9488',
+                                   corner_radius=20,
+                                   fg_color="#171717",
+                                   )
+        self.user_entry.place(x=50, y=15)
 
-        self.user = ctk.CTkEntry(self,
-                                 width=200,
-                                 height=30
-                                 )
-        self.user.place(x=20,y=5)
+        #Entry da senha
+        self.senha_entry = ctk.CTkEntry(self,
+                                   width=200,
+                                   height=40,
+                                   font=("Arial", 14),
+                                   placeholder_text_color='#0d9488',
+                                   placeholder_text="Senha",
+                                   border_width=2,
+                                   border_color='#0d9488',
+                                   corner_radius=20,
+                                   fg_color="#171717",
+                                   show="*"
+                                   )
+        self.senha_entry.place(x=50, y=90)
+
+        #Botão de recuperar senha
+        self.link_recover = ctk.CTkButton(self,
+                                    text="Esqueceu sua senha?",
+                                    font=("Arial", 13),
+                                    fg_color="transparent",
+                                    width=8,
+                                    height=8,
+                                    hover_color="#115e59",
+                                    text_color="#0d9488",
+                                    corner_radius=20
+                                    )
+        self.link_recover.place(x=55,y=140)
+
+        #Botão de fazer login
+        self.login_btn = ctk.CTkButton(self,
+                                   width=70,
+                                   height=40,
+                                   text="Login",
+                                   font=("Arial", 14),
+                                   command=lambda: print("Botão Pressionado"),
+                                   border_width=2,
+                                   corner_radius=20,
+                                   fg_color="#171717",
+                                   border_color="#0d9488",
+                                   text_color="#0d9488",
+                                   hover_color='#115e59',
+
+                                   )
+        self.login_btn.place(x=110, y=200)
+
+        #Botão de mostrar a senha   (São dois botões pois altera o ícone quando pressionado)
+        self.show_pass_btn = ctk.CTkButton(self,
+                                           text="",
+                                           image=iconspath.CLOSED_EYE_ICON,
+                                           width=10,
+                                           height=10,
+                                           corner_radius=50,
+                                           fg_color="transparent",
+                                           hover_color="#115e59",
+                                           command=self.show_hide
+                                           )
+        self.show_pass_btn.place(x=210, y=99)
+
+        #Botão de ocultar a senha
+        self.hide_pass_btn = ctk.CTkButton(self,
+                                           text="",
+                                           image=iconspath.EYE_ICON,
+                                           width=10,
+                                           height=10,
+                                           corner_radius=50,
+                                           fg_color="transparent",
+                                           hover_color="#115e59",
+                                           command=self.show_hide
+                                           )
+        self.hide_pass_btn.place(x=210, y=99)
+
+    # 
+    
+    #Função que oculta/exibe a senha e altera os ícones
+    def show_hide(self):
+        if self.senha_oculta:
+            self.senha_entry.configure(show="")
+            self.hide_pass_btn.place_forget()
+            self.show_pass_btn.place(x=210, y=99)
+            self.senha_oculta = False
+        else:
+            self.senha_entry.configure(show="*")
+            self.show_pass_btn.place_forget()
+            self.hide_pass_btn.place(x=210, y=99)
+            self.senha_oculta = True
 
 
 #Janela de funções ADM
@@ -86,14 +182,14 @@ class Gestor(tk.Tk):
 
         # Botão da barra lateral
         self.sidebar_btn = ctk.CTkButton(master=self.sidebar_bg,
-                                    width=45,
-                                    height=45,
-                                    text="",
-                                    image=iconspath.SIDEBAR_ICON,
-                                    fg_color="transparent",
-                                    hover_color="#115e59",
-                                    command=self.onclick,
-                                    )
+                                         width=45,
+                                         height=45,
+                                         text="",
+                                         image=iconspath.SIDEBAR_ICON,
+                                         fg_color="transparent",  # Transparent background
+                                         hover_color="#115e59",
+                                         command=self.onclick,
+                                         )
         self.sidebar_btn.place(x=2, y=3)
 
         # Botão de configurações
@@ -125,13 +221,13 @@ class Gestor(tk.Tk):
         self.admin_btn = ctk.CTkButton(self.sidebar_bg,
                                        width=45,
                                        height=45,
-                                       fg_color="transparent",
+                                       fg_color="transparent",  # Transparent background
                                        hover_color="#115e59",
                                        text="",
                                        image=iconspath.ADMIN_ICON,
                                        command=self.admin_cmd
                                        )
-        self.admin_btn.place(x=2,y=500)
+        self.admin_btn.place(x=2, y=500)
 
 
     def open_config(self):
