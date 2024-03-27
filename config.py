bc0728c9bc7c9e8c5e23b7ad6de07c6dbd729732
Adriel Soares <adriellsoares067@gmail.com>
2024-03-27 17:02:09 -0400

    Functions update

--- config.py
+++ config.py
@@ -2,6 +2,12 @@ import bcrypt
 import pyodbc
 from datetime import datetime
 import pytz
+import random
+import string
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from private import APP_KEY, MAIL
 
 
 #Todos as funções dos CRUDs necessários para o sistema
@@ -137,3 +143,63 @@ def timenow():
     fuso_br = pytz.timezone("America/Sao_Paulo")
     return datetime.now(fuso_br).strftime("%d/%m/%Y %H:%M:%S")
 
+def generate_token():
+    """Gera um token único de redefinição de senha
+
+    Returns:
+        Código de 5 dígitos alfanumérico
+    
+    """
+    return "".join(random.choices(string.ascii_letters.upper() + string.digits, k=5))
+
+def recover_pass(email, user):
+    code = generate_token()
+    def send_mail():
+        # Configurações do servidor SMTP do Gmail
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587  # Porta para conexão TLS
+        sender_email = MAIL  # Seu e-mail do Gmail
+        sender_password = APP_KEY   #senha do Gmail
+
+        # Destinatário e corpo do e-mail
+        recipient_email = email
+        subject = 'Redefinição de senha'
+        body = f'<p>Seu código de redefinição de senha é: <b>{code}</b></p>'
+
+        # Criar mensagem
+        message = MIMEMultipart()
+        message['From'] = sender_email
+        message['To'] = recipient_email
+        message['Subject'] = subject
+        message.attach(MIMEText(body, 'html'))
+
+        # Conectar ao servidor SMTP do Gmail e enviar e-mail
+        try:
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            server.starttls()  # Habilitar TLS
+            server.login(sender_email, sender_password)
+            text = message.as_string()
+            server.sendmail(sender_email, recipient_email, text)
+            print('E-mail enviado com sucesso!')
+        except Exception as e:
+            print(f'Erro ao enviar e-mail: {str(e)}')
+        finally:
+            server.quit()  # Encerrar conexão com o servidor SMTP
+
+    def insert_code():
+        try:
+            dados_conexao = ("Driver={SQLite3 ODBC Driver};"
+                    "Server=localhost;"
+                    "Database=gerenciador.db")
+            conexao = pyodbc.connect(dados_conexao)
+
+            cursor = conexao.cursor()
+            cursor.execute("UPDATE admins SET [Codigo Temporario] = ? WHERE Email = ? AND Usuario = ?", (code, email, user))
+            cursor.commit()
+            cursor.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    if insert_code():
+        send_mail()
