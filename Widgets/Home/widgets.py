b9942d14bb2e481ca306ae735a288c18d71a2f68
Adriel Soares <adriellsoares067@gmail.com>
2024-04-16 09:51:37 -0400

    Homepage update

--- Widgets/Home/widgets.py
+++ Widgets/Home/widgets.py
@@ -4,17 +4,19 @@ def home_widgets(master):
 
     #Frame do cabeçalho
     header_frame = ctk.CTkFrame(master,
-                                            width=1365,
-                                            height=50,
-                                            fg_color="#14b8a6",
+                                            width=1366,
+                                            height=52,
+                                            fg_color="#0891b2",
+                                            border_width=2,
+                                            border_color="black"
                                             )
-    header_frame.place(x=40, y=0)
+    header_frame.place(x=39, y=-2)
 
     #Texto do cabeçalho
     lb1 = ctk.CTkLabel(header_frame,
                                     text="Funcionários",
                                     font=("Roboto", 30, "bold"),
-                                    text_color="#171717"
+                                    text_color="white"
                                     )
     lb1.place(x=620,y=7)
 
@@ -22,51 +24,57 @@ def home_widgets(master):
     stat_frame = ctk.CTkFrame(master,
                           width=455,
                           height=668,
-                          fg_color="red"
+                          fg_color="red",
+                          corner_radius=0
                           )
-    stat_frame.place(x=40,y=51)
+    stat_frame.place(x=40,y=50)
 
     #Frame do gráfico de gêneros
     gen_frame = ctk.CTkFrame(master,
-                             width=455,
-                             height=333,
-                             fg_color="green"
+                             width=456,
+                             height=334,
+                             fg_color="green",
+                             corner_radius=0
                              )
-    gen_frame.place(x=494,y=51)
+    gen_frame.place(x=494,y=50)
 
     #Frame do gráfico de idades
     age_frame = ctk.CTkFrame(master,
-                             width=455,
-                             height=333,
-                             fg_color="blue"
+                             width=456,
+                             height=336,
+                             fg_color="blue",
+                             corner_radius=0
                              )
-    age_frame.place(x=494,y=385)
+    age_frame.place(x=494,y=382)
 
     #Frame do gráfico de áreas
     area_frame = ctk.CTkFrame(master,
                              width=455,
-                             height=333,
-                             fg_color="orange"
+                             height=334,
+                             fg_color="orange",
+                             corner_radius=0
                              )
-    area_frame.place(x=949,y=51)
+    area_frame.place(x=949,y=50)
 
     #Frame do gráfico de cargos
     pos_frame = ctk.CTkFrame(master,
-                             width=455,
-                             height=333,
-                             fg_color="purple"
+                             width=456,
+                             height=336,
+                             fg_color="purple",
+                             corner_radius=0
                              )
-    pos_frame.place(x=949,y=385)
+    pos_frame.place(x=949,y=382)
 
     #Botão de atualizar as informações
     ref_btn = ctk.CTkButton(header_frame,
                             text="Atualizar",
                             command=lambda: print("Atualizar"),
-                            font=("Roboto", 16),
+                            font=("Roboto", 16, "bold"),
                             width=90,
                             height=35,
                             corner_radius=20,
                             border_width=2,
-                            border_color="black"
+                            border_color="#FB9C8D",
+                            fg_color="#171717"
                             )
     ref_btn.place(x=1200,y=8)
